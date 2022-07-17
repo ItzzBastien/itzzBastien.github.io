c2e14500db971224f8755ec87e803e6aa1dfe39e
Thekillerazert <hutin.bastien973@gmail.com>
2022-07-17 12:21:28 -0300

    updated to a more user friendly waiting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import './App.css'
+import { Spinner } from '@chakra-ui/react';
+
 export default function App() {
 
   return (
-    <div>
-      waiting
+    <div className="bg">
+      <div className="loading-content">
+        <a href="https://github.com/itzzbastien">
+          <h1>Loading content</h1>
+        </a>
+        <Spinner size={"xl"} speed="1s" color='#78B2BF'/>
+        
+      </div>
     </div>
   );
 }
